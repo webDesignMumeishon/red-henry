4f10fc6a61f0e5fe9d834120f5a930f55ffaf12b
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-09-14 02:20:24 -0300

    url *

--- app-backend/app.js
+++ app-backend/app.js
@@ -33,7 +33,7 @@ const store = new session.MemoryStore
 var app = express();
 
 app.use(cors({
-  origin: "https://find-closest-henry.netlify.app",
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 }))
